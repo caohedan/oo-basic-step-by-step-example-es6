cbbd3a4a1dfce303984f21d59377e792be105acb
caohedan <m18390990364@163.com>
2018-07-09 19:28:24 +0800

    finish the practice-10

--- src/practice_10/class.js
+++ src/practice_10/class.js
@@ -1,1 +1,32 @@
+"use strict";
+export default class Class{
+    constructor(number){
+        this.number=number;
+        this.leader;
+    }
+    getNum(){
+        return this.number;
+    }
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+
+    equal(klass) {
+        return klass && this.number == klass.number;
+    }
+    isLeader(student){
+            return this.leader && student && student.equal(this.leader);
+    }
+    assignLeader(student) {
+        if (student && student.klass.equal(this)) {
+            this.leader = student;
+        } else {
+            console.log("It is not one of us.");
+        }
+    }
+
+    appendMember(student) {
+        student.klass = this;
+    }
+}
 
--- src/practice_10/student.js
+++ src/practice_10/student.js
@@ -1,1 +1,25 @@
+"use strict";
+import Person from "../../src/practice_10/person.js";
+export default  class Student extends Person{
+    constructor(id, name, age, klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    introduce() {
+        if (this.klass.isLeader(this)) {
+            return `${super.introduce()} I am a Student. I am Leader of ${this.klass.getDisplayName()}.`;
+        }
+        return `${super.introduce()} I am a Student. I am at ${this.klass.getDisplayName()}.`;
+    }
+    getName(){
+        return this.name;
+    }
+    getClass()
+    {
+        return this.klass;
+    }
+    equal(student) {
+        return student && this.id === student.id;
+    }
+}
 
