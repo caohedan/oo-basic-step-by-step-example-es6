705e45b4b14d97246907730623f01dfcaaa4b324
caohedan <m18390990364@163.com>
2018-07-09 16:33:56 +0800

    update some detail

--- src/practice_5/worker.js
+++ src/practice_5/worker.js
@@ -1,5 +1,5 @@
 "use strict";
-import Person from "../../src/practice_4/person.js";
+import Person from "../../src/practice_5/person.js";
 export default class Worker extends Person{
     constructor(name,age)
     {
